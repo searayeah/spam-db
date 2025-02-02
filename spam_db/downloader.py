1e98afe261e5be048eae0750ce2c05c2452db468
searayeah <insslava@gmail.com>
2025-02-02 18:38:23 +0300

    Initial

--- spam_db/downloader.py
+++ spam_db/downloader.py
@@ -10,7 +10,7 @@ def download_and_extract_zip(url: str, extract_to: str) -> None:
     try:
         os.makedirs(extract_to, exist_ok=True)
 
-        response = requests.get(url)
+        response = requests.get(url, timeout=30)
         response.raise_for_status()
 
         with zipfile.ZipFile(BytesIO(response.content)) as zip_ref:
@@ -19,7 +19,3 @@ def download_and_extract_zip(url: str, extract_to: str) -> None:
         print(f"ZIP file successfully downloaded and extracted to {extract_to}")
     except Exception as e:
         print(f"Error: {e}")
-
-
-# https://yacb.lom.name/srvapp/get-database/sia.zip
-# https://gitlab.com/xynngh/YetAnotherCallBlocker_data/raw/zip_v1/archives/sia.zip
